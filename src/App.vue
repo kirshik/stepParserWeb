d00fe07cf7e296ca8aada17663d2ff64b73ce599
kirshik <dhdjsjsdj@gmail.com>
2024-06-09 23:03:05 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+import block from "./components/block.vue";
+import data from "./components/STRUCTURE_Final.json";
+import { ref } from "vue";
+
+const description = ref(data[Object.keys(data)[0]].description);
+const pickedKey = ref<string>(Object.keys(data)[0]);
+const handleClick = (item, key) => {
+  pickedKey.value = key;
+  description.value = item.description;
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="left-sec">
+      <ul>
+        <block
+          :accent="pickedKey"
+          :data="data"
+          v-slot="slotProps"
+          @handleClick="(item, key) => handleClick(item, key)"
+        >
+          <block
+            :accent="pickedKey"
+            :data="slotProps?.child"
+            v-slot="slotPropsChild"
+            @handleClick="(item, key) => handleClick(item, key)"
+          >
+            <block
+              :accent="pickedKey"
+              :data="slotPropsChild?.child"
+              v-slot="slotPropsChild2"
+              @handleClick="(item, key) => handleClick(item, key)"
+            >
+              <block
+                :accent="pickedKey"
+                :data="slotPropsChild2?.child"
+                v-slot="slotPropsChild3"
+                @handleClick="(item, key) => handleClick(item, key)"
+              >
+                <block
+                  :accent="pickedKey"
+                  :data="slotPropsChild3?.child"
+                  v-slot="slotPropsChild4"
+                  @handleClick="(item, key) => handleClick(item, key)"
+                >
+                  <block
+                    :accent="pickedKey"
+                    :data="slotPropsChild4?.child"
+                    v-slot="slotPropsChild5"
+                  >
+                    <block
+                      :accent="pickedKey"
+                      :data="slotPropsChild5?.child"
+                      v-slot="slotPropsChild6"
+                    >
+                      <block
+                        :accent="pickedKey"
+                        :data="slotPropsChild6?.child"
+                      />
+                    </block>
+                  </block>
+                </block>
+              </block>
+            </block>
+          </block>
+        </block>
+      </ul>
+    </div>
+    <div class="right-sec">{{ description }}</div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: grid;
+  grid-template-columns: min-content 1fr;
+  min-width: 80vw;
+  gap: 10px;
+  border-collapse: collapse;
+  border: 1px solid black;
+  background-color: wheat;
+  height: 100%;
+  overflow: hidden;
+}
+.left-sec,
+.right-sec {
+  width: 100%;
+  height: 100%;
+  background-color: white;
+}
+.right-sec {
+  padding-left: 20px;
+  text-align: start;
+  word-wrap: keep-all;
+  white-space: pre-wrap;
+}
+</style>
---
+++ src/components/block.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="left-panel">
+    <li v-for="(item, key) in data" :key="key">
+      <span
+        @click="emit('handleClick', item, key)"
+        :class="{ accent: accent == key }"
+        >{{ key }}</span
+      >
+      <ul>
+        <li
+          v-if="item.childAmount > 0"
+          v-for="(child, childKey) in item.children"
+          :key="childKey"
+        >
+          <slot :child="child"></slot>
+        </li>
+      </ul>
+    </li>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from "vue";
+interface StpParserBlockProps {
+  data: any;
+  accent: string;
+}
+const props = defineProps<StpParserBlockProps>();
+const emit = defineEmits<{
+  (e: "handleClick", item: unknown, key: string): void;
+}>();
+const { data, accent } = toRefs(props);
+</script>
+
+<style scoped>
+.left-panel {
+  width: fit-content;
+  min-width: 20vw;
+  text-align: start;
+  height: 100%;
+}
+li {
+  white-space: nowrap;
+  word-break: keep-all;
+  list-style: none;
+}
+.accent {
+  font-weight: bold;
+}
+</style>
